4d1c186675283bce6f9f646cf21fa69223429a44
Dmitriy V. Isaev <shtirlizlive@yandex.ru>
2026-05-15 08:50:10 +0300

    fix validate password

--- src/lib/model/User.svelte.ts
+++ src/lib/model/User.svelte.ts
@@ -36,7 +36,7 @@ export type LogoutResponse = {
 }
 
 export const userNameValidateRegExp = v.regex(/^[a-zA-Z][a-zA-Z0-9_]{2,15}$/, 'Начинается с буквы, за которой следуют буквы, цифры или подчеркивания.');
-export const passwordValidateRegExp = v.regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, "Минимум 8 символов, как минимум одна заглавная буква, одна строчная буква, одна цифра и один специальный символ.");
+export const passwordValidateRegExp = v.regex(/^.{4,}$/, "Минимум 4 символов.");
 
 export const LoginSchema = v.object({
     userName: v.pipe(
